37aac20c7b1e01b7aeb15ca7c38d510ec4a08296
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-04-04 10:20:02 -0300

    Utilizando Logger

--- src/main/java/com/talissonmelo/service/HelloWorldService.java
+++ src/main/java/com/talissonmelo/service/HelloWorldService.java
@@ -1,9 +1,22 @@
 package com.talissonmelo.service;
 
+import com.talissonmelo.Application;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.inject.Singleton;
 
 @Singleton
 public class HelloWorldService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldService.class);
+
+    @EventListener
+    public void onStart(StartupEvent event){
+        LOG.debug("Startup: {}", HelloWorldService.class.getSimpleName());
+    }
 
     public String sayHi(){
         return "Hello World From Service";
